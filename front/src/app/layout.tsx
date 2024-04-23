e4d742170cedb827b6f830079e2196563c47b955
zoowb <qlenfrl101@naver.com>
2024-04-23 16:10:48 +0900

    style [FE] add NotoSansKR Font

--- front/src/app/layout.tsx
+++ front/src/app/layout.tsx
@@ -1,9 +1,16 @@
+import { Noto_Sans_KR } from 'next/font/google'
 import * as styles from '@/styles/global.css'
 
+const notoSansKr = Noto_Sans_KR({ subsets: ['cyrillic'] })
 
-export default function RootLayout({ children}: Readonly<{ children: React.ReactNode;}>) {
+export default function RootLayout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en">
-      <body className={styles.container}>{children}</body>
-    </html>)
+      <body className={`${notoSansKr.className} ${styles.container}`}>
+        {children}
+      </body>
+    </html>
+  )
 }
