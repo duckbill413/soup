c04df157e1547a38d0c158a64d9d154f7a9228f0
정승원 <stevenjung1213@gmail.com>
2024-04-23 17:51:30 +0900

    feat [FE] main page published
    메인 페이지 1차 퍼블리싱

---
+++ front/src/components/mainpage/thirdMain.tsx
@@ -0,0 +1,17 @@
+
+function ThirdMain() {
+    return (
+      <div style={{
+          height: "90vh",
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+          backgroundColor: '#f4cfdf'
+      }}>
+          3
+      </div>
+    )
+}
+
+export default ThirdMain
