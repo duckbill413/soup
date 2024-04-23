38d38b000fc0ea56424e283d7249e43f175b9722
duckbill413 <uhyeon7399@daum.net>
2024-04-24 01:49:21 +0900

    feat [BE] 프로젝트 생성 DTO Swagger 설명

--- back/soup-api/src/main/java/io/ssafy/soupapi/domain/project/usecase/dto/request/CreateProjectDto.java
+++ back/soup-api/src/main/java/io/ssafy/soupapi/domain/project/usecase/dto/request/CreateProjectDto.java
@@ -1,10 +1,14 @@
 package io.ssafy.soupapi.domain.project.usecase.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotEmpty;
 
+@Schema(description = "프로젝트 생성 요청")
 public record CreateProjectDto(
         @NotEmpty(message = "프로젝트 이름을 지정해 주세요.")
+        @Schema(description = "프로젝트 이름")
         String name,
+        @Schema(description = "프로젝트 이미지")
         String imgUrl
 ) {
 }
