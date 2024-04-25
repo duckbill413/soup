e06aed4a50b0acf47160ec81ee04fecabdb9c559
Yoosuean <awetumnn@gmail.com>
2024-04-25 16:19:38 +0900

    fix [BE] swagger api 테스트시 http로 전송되는 이슈 수정

--- back/soup-api/src/main/java/io/ssafy/soupapi/SoupApiApplication.java
+++ back/soup-api/src/main/java/io/ssafy/soupapi/SoupApiApplication.java
@@ -1,9 +1,16 @@
 package io.ssafy.soupapi;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
+@OpenAPIDefinition(
+        servers = {
+                @Server(url="https://be-api.so-up.store", description="Default Server url")
+        }
+)
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class SoupApiApplication {
 
