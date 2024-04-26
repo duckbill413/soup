4919eee6df8ad53b059eacb6088fa472b215953a
고석주 <hanjooseok00@gmail.com>
2024-04-26 16:30:44 +0900

    chore [FE] next config 설정 변경
    변경에 따른 코드 변경

--- front/src/containers/Project.tsx
+++ front/src/containers/Project.tsx
@@ -3,23 +3,27 @@
 import { ProjectMemberImageUri, ProjectRes } from '@/types/project'
 import * as styles from '@/containers/project.css'
 import { useRouter } from 'next/navigation'
+import Image from 'next/image'
 
-const PROJECT_IMAGE_WIDTH = 450;
-const PROJECT_IMAGE_HIGHT = 300;
-const MEMBER_IMAGE = 44;
+const PROJECT_IMAGE_WIDTH = 450
+const PROJECT_IMAGE_HIGHT = 300
+const MEMBER_IMAGE = 44
 
 export default function Project({ projectId, projectImageUri, projectName, projectMemberImageUris }: ProjectRes) {
-  const router = useRouter();
+  const router = useRouter()
   return (
     <div className={styles.container}>
-      <button onClick={()=>router.push(`/project/${projectId}/outline`)} type="button">
-        <img className={styles.projectImage} src={projectImageUri} alt="프로젝트 이미지" width={PROJECT_IMAGE_WIDTH} height={PROJECT_IMAGE_HIGHT} />
+      <button onClick={() => router.push(`/project/${projectId}/outline`)} type="button">
+        {projectImageUri &&
+          <Image unoptimized className={styles.projectImage} src={projectImageUri} alt="프로젝트 이미지" width={PROJECT_IMAGE_WIDTH}
+                 height={PROJECT_IMAGE_HIGHT} />}
         <p className={styles.projectName}>{projectName}</p>
       </button>
       <div>
         {projectMemberImageUris?.map((member: ProjectMemberImageUri) =>
           member.projectMemberImageUri &&
-          <img src={member.projectMemberImageUri} alt="프로젝트 멤버 이미지" width={MEMBER_IMAGE} height={MEMBER_IMAGE} />)
+          <Image unoptimized src={member.projectMemberImageUri} alt="프로젝트 멤버 이미지" width={MEMBER_IMAGE} height={MEMBER_IMAGE} />)
+
         }
       </div>
     </div>
