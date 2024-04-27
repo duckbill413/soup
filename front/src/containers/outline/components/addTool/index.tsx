07088e5905058aaec1bd6f8df0fa75aa9cd832e8
정승원 <129469541+JungSwww1@users.noreply.github.com>
2024-04-27 23:38:34 +0900

    feat [FE] outline tool modal published
    - 프로젝트 개요 툴 추가 모달 퍼블리시

--- front/src/containers/outline/components/addTool/index.tsx
+++ front/src/containers/outline/components/addTool/index.tsx
@@ -5,6 +5,7 @@ import { useState } from 'react'
 import Add from '#/assets/icons/outline/addButton.svg'
 import ToolTable from '@/containers/outline/components/addTool/ToolTable'
 import * as styles from "@/containers/outline/styles/addTool/outlineAddTool.css"
+import OutlineToolModal from '@/containers/outline/components/modals/addTool'
 
 interface RoleTableType {
   name: string;
@@ -13,12 +14,15 @@ interface RoleTableType {
 
 function OutlineAddTool () {
   const [rows, setRows] = useState<RoleTableType[]>([]);
+  const [showModal, setShowModal] = useState(false)
+  const clickModal = () => setShowModal(!showModal)
   const addTool = () => {
     const newRow = {
       name: `Tool ${rows.length + 1}`,
       description: 'https://naver.com',
     };
     setRows([...rows, newRow]);
+    setShowModal(!showModal)
   };
 
   return (
@@ -28,6 +32,7 @@ function OutlineAddTool () {
         <Image src={Add} alt="add" width={32} height={32} onClick={addTool} />
       </div>
       <ToolTable rows={rows} />
+      {showModal && <OutlineToolModal clickModal={clickModal}/>}
     </div>
   )
 }
