dad01838ab3499e1c73c2b84437111106eae08be
최지우 <qlenfrl101@naver.com>
2024-04-30 16:03:37 +0900

    feat [FE] 알림 화면 퍼블리싱

--- front/src/components/Header/Header.tsx
+++ front/src/components/Header/Header.tsx
@@ -1,28 +1,41 @@
-import * as styles from '@/components/Header/header.css'
-import Image from 'next/image'
-import { faker } from '@faker-js/faker'
 import voicechat from '@/../public/assets/icons/voicechat.svg'
-import Notification from '@/../public/assets/icons/notification'
+import * as styles from '@/components/Header/header.css'
 import Logo from '@/components/Logo/Logo'
+import Notifications from '@/containers/Notifications'
 import vars from '@/styles/variables.css'
+import { faker } from '@faker-js/faker'
+import Image from 'next/image'
 
 const projectName = '스타트 프로젝트, 스프'
 
-type Props={
-  theme: 'white'|'black';
-  useVoice: boolean;
+type Props = {
+  theme: 'white' | 'black'
+  useVoice: boolean
 }
-export default function Header({theme,useVoice}:Props) {
+export default function Header({ theme, useVoice }: Props) {
   return (
-    <div className={`${theme==='white' ? styles.whiteTheme : ""} ${styles.container}`}>
+    <div
+      className={`${theme === 'white' ? styles.whiteTheme : ''} ${styles.container}`}
+    >
       <div>
-        <Logo logoColor={theme==='white' ? 'black' : vars.color.white} leafColor={theme==='white' ? 'black' : vars.color.lightGreen} />
+        <Logo
+          logoColor={theme === 'white' ? 'black' : vars.color.white}
+          leafColor={theme === 'white' ? 'black' : vars.color.lightGreen}
+        />
         <p>{projectName}</p>
       </div>
       <div className={styles.assistant}>
-        {useVoice && <Image src={voicechat} width={44} height={44} alt="음성 채팅" />}
-        <Notification color={theme ==='white' ? 'black' : 'white'}/>
-        <Image unoptimized src={faker.image.avatar()} width={44} height={44} alt="프로필" />
+        {useVoice && (
+          <Image src={voicechat} width={44} height={44} alt="음성 채팅" />
+        )}
+        <Notifications theme={theme} />
+        <Image
+          unoptimized
+          src={faker.image.avatar()}
+          width={44}
+          height={44}
+          alt="프로필"
+        />
       </div>
     </div>
   )
