be9e3dd40ee224db934fa693f6651ea7367849e1
최지우 <qlenfrl101@naver.com>
2024-05-03 14:45:35 +0900

    fix [FE] align header

--- front/src/containers/main/styles/mainPage.css.ts
+++ front/src/containers/main/styles/mainPage.css.ts
@@ -1,5 +1,5 @@
-import { style, keyframes } from '@vanilla-extract/css';
 import vars from '@/styles/variables.css'
+import { keyframes, style } from '@vanilla-extract/css'
 
 // 애니메이션 정의
 const bounce = keyframes({
@@ -11,7 +11,7 @@ const bounce = keyframes({
     transform: 'translateY(0)',
     animationTimingFunction: 'cubic-bezier(0, 0, 0.2, 1)',
   },
-});
+})
 
 // bounce 스타일 생성
 export const bounceAnimation = style({
@@ -20,25 +20,26 @@ export const bounceAnimation = style({
   padding: 0,
   background: 'none',
   border: 'none',
-  position:'absolute',
-  bottom:'0'
-});
-
-export const pageContainer = style ({
-  height:'90vh',
-  width:'100%',
-  overflowY:'auto'
+  position: 'absolute',
+  bottom: '0',
 })
 
-export const mainPageHeader = style ({
+export const pageContainer = style({
+  height: '90vh',
+  width: '100%',
+  overflowY: 'auto',
+})
+
+export const mainPageHeader = style({
   display: 'flex',
   justifyContent: 'space-between',
+  alignItems: 'center',
   height: '10vh',
   width: '95%',
   marginLeft: '2%',
 })
 
-export const mainPageHeaderBtn = style ({
+export const mainPageHeaderBtn = style({
   padding: 0,
   background: 'none',
   border: 'none',
