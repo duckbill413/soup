1b8b4fbbbe5b6c1d90b3ab1caa1e55b826a81ee3
최지우 <qlenfrl101@naver.com>
2024-05-03 14:54:57 +0900

    fix [FE] env 출력 추가

--- front/src/app/layout.tsx
+++ front/src/app/layout.tsx
@@ -10,6 +10,8 @@ export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   console.log('env==', process.env.NEXT_PUBLIC_LIVEBLOCKS_PUBLIC_KEY)
+  console.log('host==', process.env.NEXT_PUBLIC_SERVER_HOST)
+  console.log('baseURL==', process.env.NEXT_PUBLIC_BACKEND_BASE_URL)
 
   return (
     <html lang="en">
