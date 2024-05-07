849e805b66ab870687d48c40b3b0edae80b6f7ef
Yoosuean <awetumnn@gmail.com>
2024-05-07 14:17:44 +0900

    feat [BE] Openvidu 음성채팅 세션 생성/삭제 및 토큰 생성 API 추가

---
+++ back/soup-api/src/main/java/io/ssafy/soupapi/domain/openvidu/dto/response/UserConnection.java
@@ -0,0 +1,16 @@
+package io.ssafy.soupapi.domain.openvidu.dto.response;
+
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserConnection {
+    private String sessionId;
+    private String token;
+    private String connectionId;
+    private Long createdAt;
+}
