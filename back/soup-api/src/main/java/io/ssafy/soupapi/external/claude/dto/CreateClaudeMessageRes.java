ea0f74c710b9f5189657d00cbaa9f14c4a948e62
이수현 <olafminion0116@naver.com>
2024-05-07 17:20:26 +0900

    refactor [BE] class -> record로 변환

--- back/soup-api/src/main/java/io/ssafy/soupapi/external/claude/dto/CreateClaudeMessageRes.java
+++ back/soup-api/src/main/java/io/ssafy/soupapi/external/claude/dto/CreateClaudeMessageRes.java
@@ -2,46 +2,38 @@ package io.ssafy.soupapi.external.claude.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import io.ssafy.soupapi.domain.project.usecase.dto.CreateAiProposal;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.Builder;
 
 import java.util.List;
 import java.util.StringTokenizer;
 
-@ToString
-@Getter @Setter
-public class CreateClaudeMessageRes {
-    String id;
-    String type;
-    String role;
+@Builder
+public record CreateClaudeMessageRes(
+    String id,
+    String type,
+    String role,
 
-    String model;
-    @JsonProperty("stop_sequence")
-    String stopSequence;
-    Usage usage;
-    List<Content> content;
-    @JsonProperty("stop_reason")
-    String stopReason;
+    String model,
+    @JsonProperty("stop_sequence") String stopSequence,
+    Usage usage,
+    List<Content> content,
+    @JsonProperty("stop_reason") String stopReason
+){
 
-    @ToString
-    @Getter @Setter
-    static class Content {
-        String text;
-        String type;
+    public record Content(
+        String text,
+        String type
+    ) {
     }
 
-    @ToString
-    @Getter @Setter
-    static class Usage {
-        @JsonProperty("input_tokens")
-        Integer inputTokens;
-        @JsonProperty("output_tokens")
-        Integer outputTokens;
+    public record Usage(
+        @JsonProperty("input_tokens") Integer inputTokens,
+        @JsonProperty("output_tokens") Integer outputTokens
+    ) {
     }
 
     public CreateAiProposal toResponse() {
-        String claudeRes = this.content.get(0).getText();
+        String claudeRes = this.content.get(0).text;
         StringTokenizer st = new StringTokenizer(claudeRes, "\n");
 
         String background = st.nextToken().substring(2);
--- back/soup-api/src/main/java/io/ssafy/soupapi/external/claude/dto/MessageDto.java
+++ back/soup-api/src/main/java/io/ssafy/soupapi/external/claude/dto/MessageDto.java
@@ -1,47 +1,21 @@
 package io.ssafy.soupapi.external.claude.dto;
 
-import io.ssafy.soupapi.domain.project.usecase.dto.CreateAiProposal;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.Builder;
 
-import java.util.List;
+@Builder
+public record MessageDto (
+        String role,
+        String content
+) {
 
-@ToString
-@Getter
-@Setter
-public class MessageDto {
-    String role = "user";
-    String content;
-
-    public MessageDto(CreateAiProposal req) {
-        StringBuilder sb = new StringBuilder();
-
-        // 기획 배경
-        sb.append("I need an idea for my IT service development project. I want to start from ");
-        sb = appendKeywords(sb, req.background());
-
-        // 서비스 소개
-        sb.append("I want my service related to ");
-        sb = appendKeywords(sb, req.intro());
-
-        // 서비스 타겟
-        sb.append("I want to target ");
-        sb = appendKeywords(sb, req.target());
-
-        // 기대 효과
-        sb.append("I hope users to ");
-        sb = appendKeywords(sb, req.result());
-
-        this.content = sb.toString();
+    public static class MessageDtoBuilder {
+        private String role = "user";
     }
 
-    // {'a', 'b'. } 형태로 append 된다.
-    private StringBuilder appendKeywords(StringBuilder sb, List<String> keywordList) {
-        for (String keyword : keywordList) {
-            sb.append("'").append(keyword).append("', ");
-        }
-        sb.replace(sb.length() - 2, sb.length() - 1, ".");
-        return sb;
+    public CreateClaudeMessageReq toCreateClaudeMessageReq() {
+        CreateClaudeMessageReq claudeReq = CreateClaudeMessageReq.builder().build();
+        claudeReq.addMessage(this);
+        return claudeReq;
     }
+
 }
