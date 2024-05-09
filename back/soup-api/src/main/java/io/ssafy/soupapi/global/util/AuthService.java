464b8bf9a1a02a3eca2227337460d49632848933
이수현 <olafminion0116@naver.com>
2024-05-09 10:18:50 +0900

    feat [BE] 프로젝트 권한 존재 여부 에러 처리

--- back/soup-api/src/main/java/io/ssafy/soupapi/global/util/AuthService.java
+++ back/soup-api/src/main/java/io/ssafy/soupapi/global/util/AuthService.java
@@ -1,11 +1,12 @@
 package io.ssafy.soupapi.global.util;
 
-import io.ssafy.soupapi.domain.member.dao.MemberRepository;
 import io.ssafy.soupapi.domain.member.entity.Member;
 import io.ssafy.soupapi.domain.project.postgresql.entity.Project;
 import io.ssafy.soupapi.domain.project.postgresql.entity.ProjectRole;
 import io.ssafy.soupapi.domain.projectauth.dao.ProjectAuthRepository;
 import io.ssafy.soupapi.domain.projectauth.entity.ProjectAuth;
+import io.ssafy.soupapi.global.common.code.ErrorCode;
+import io.ssafy.soupapi.global.exception.BaseExceptionHandler;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
@@ -18,7 +19,8 @@ import java.util.UUID;
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class AuthService {
-    private final MemberRepository memberRepository;
+
+    private final FindEntityUtil findEntityUtil;
     private final ProjectAuthRepository projectAuthRepository;
     private static final ProjectRole[] PRIMARY_PROJECT_ROLE = {ProjectRole.ADMIN, ProjectRole.MAINTAINER};
     private static final ProjectRole VIEWER_PROJECT_ROLE = ProjectRole.VIEWER;
@@ -31,11 +33,14 @@ public class AuthService {
      * @return
      */
     public boolean hasProjectRoleMember(String projectId, UUID memberId) {
-        var projectAuths = projectAuthRepository.findByMemberAndProject(
-                Member.builder().id(memberId).build(),
-                Project.builder().id(projectId).build()
-        );
-        return !projectAuths.isEmpty();
+        Member member = findEntityUtil.findMemberById(memberId);
+        Project project = findEntityUtil.findPProjectById(projectId);
+        var projectAuths = projectAuthRepository.findByMemberAndProject(member, project);
+
+        if (projectAuths.isEmpty()) {
+            throw new BaseExceptionHandler(ErrorCode.NOT_FOUND_PROJECT_AUTH);
+        }
+        return true;
     }
 
     public boolean hasPrimaryProjectRoleMember(String projectId, UUID memberId) {
