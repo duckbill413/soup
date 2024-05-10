4a03c3a6d7ef62ed8d05bcf067cb8063ca0b31ab
최지우 <qlenfrl101@naver.com>
2024-05-10 14:24:15 +0900

    fix [FE] api페이지에 cursor 추가

--- front/src/app/(after-auth)/project/[projectId]/api/page.tsx
+++ front/src/app/(after-auth)/project/[projectId]/api/page.tsx
@@ -1,32 +1,35 @@
 import { ErrorIcon, FilterIcon } from '@/../public/assets/icons'
 import Room from '@/app/(after-auth)/project/[projectId]/api/Room'
-import { StepTitleWithGuide } from '@/components/StepTitle/StepTitle'
-import APITable from '@/containers/api/components/APITable'
 import IconButton from '@/components/IconButton'
+import { StepTitleWithGuide } from '@/components/StepTitle/StepTitle'
+import Live from '@/components/cursor/Live'
+import APITable from '@/containers/api/components/APITable'
 import * as style from '@/containers/api/styles/index.css'
 
 export default function ApiSpecification() {
   return (
     <Room>
-      <div>
-        <StepTitleWithGuide
-          stepNum={6}
-          title="API 명세서"
-          desc="ERD를 기반으로 API 명세서를 작성해보세요."
-          guideTitle="API 명세서 작성 가이드"
-        />
-        <div className={style.buttonGroup}>
-          <IconButton name="정렬 기준" eventHandler="/">
-            <FilterIcon color="currentColor" />
-          </IconButton>
-          <div className={style.right}>
-            <IconButton name="에러 처리" eventHandler="api/error">
-              <ErrorIcon color="currentColor" />
+      <Live>
+        <div>
+          <StepTitleWithGuide
+            stepNum={6}
+            title="API 명세서"
+            desc="ERD를 기반으로 API 명세서를 작성해보세요."
+            guideTitle="API 명세서 작성 가이드"
+          />
+          <div className={style.buttonGroup}>
+            <IconButton name="정렬 기준" eventHandler="/">
+              <FilterIcon color="currentColor" />
             </IconButton>
+            <div className={style.right}>
+              <IconButton name="에러 처리" eventHandler="api/error">
+                <ErrorIcon color="currentColor" />
+              </IconButton>
+            </div>
           </div>
+          <APITable />
         </div>
-        <APITable />
-      </div>
+      </Live>
     </Room>
   )
 }
