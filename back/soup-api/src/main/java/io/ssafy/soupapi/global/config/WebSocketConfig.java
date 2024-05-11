c7f19bdc17a11d29e29207bd765e7bfbce737f7a
이수현 <olafminion0116@naver.com>
2024-05-11 15:51:35 +0900

    feat [BE] stomp 연결 허용 origin 추가 및 SockJS 제외

--- back/soup-api/src/main/java/io/ssafy/soupapi/global/config/WebSocketConfig.java
+++ back/soup-api/src/main/java/io/ssafy/soupapi/global/config/WebSocketConfig.java
@@ -18,15 +18,18 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     private final WebSocketPreHandler webSocketPreHandler;
 
     // 웹 소켓 연결을 위한 엔드포인트 설정 및 stomp sub/pub 엔드포인트 설정
+    // withSockJS() (O) -> SockJS & STOMP 커넥션으로 http://localhost:8080/ws-stomp 로 요청
+    // withSockJS() (X) -> STOMP만의 커넥션으로 ws://localhost:8080/ws-stomp로 요청
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        // stomp 접속 주소 url => /ws-stomp
         registry.addEndpoint("/ws-stomp") // {context-path}/ws-stomp로 들어온 HTTP 연결을 WebSocket 연결로 업그레이드
                 .setAllowedOriginPatterns(
                     "http://localhost:8080",
-                    "https://jiangxy.github.io"
-                ) // 연결될 엔드포인트
-                .withSockJS(); // SocketJS 를 연결한다는 설정 -> 버전 낮은 브라우저에서도 적용 가능
+                    "https://jiangxy.github.io",
+                    "http://localhost:3000",
+                    "https://so-up.store"
+                ); // 연결될 엔드포인트
+                // .withSockJS(); // SocketJS 를 연결한다는 설정 -> 버전 낮은 브라우저에서도 적용 가능
         ;
     }
 
