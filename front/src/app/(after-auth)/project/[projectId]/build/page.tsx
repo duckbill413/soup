ec958c0ac63b2941961de722d6a81291897dd1b9
최지우 <qlenfrl101@naver.com>
2024-05-13 14:25:16 +0900

    feat [FE] build 페이지 liveblocks 연결

--- front/src/app/(after-auth)/project/[projectId]/build/page.tsx
+++ front/src/app/(after-auth)/project/[projectId]/build/page.tsx
@@ -1,27 +1,33 @@
 import { StepTitleWithGuide } from '@/components/StepTitle/StepTitle'
+import Live from '@/components/cursor/Live'
 import { Metadata, RadioSection } from '@/containers/build'
-import Dependencies from '@/containers/build/Dependencies'
-import Generate from '@/containers/build/Generate'
-import * as styles from '@/containers/build/page.css'
+import Dependencies from '@/containers/build/components/Dependencies'
+import Generate from '@/containers/build/components/Generate'
+import * as styles from '@/containers/build/styles/page.css'
+import Room from './Room'
 
 export default function Build() {
   return (
-    <div>
-      <StepTitleWithGuide
-        stepNum={7}
-        title="Build"
-        desc="ERD와 API 명세서를 기반으로 프로젝트를 빌드합니다."
-        guideTitle="빌드 가이드"
-      />
-      <div className={styles.build}>
-        <RadioSection />
-        <hr className={styles.line} />
-        <section className={styles.row}>
-          <Metadata />
-          <Dependencies />
-        </section>
-      </div>
-      <Generate />
-    </div>
+    <Room>
+      <Live>
+        <div>
+          <StepTitleWithGuide
+            stepNum={7}
+            title="Build"
+            desc="ERD와 API 명세서를 기반으로 프로젝트를 빌드합니다."
+            guideTitle="빌드 가이드"
+          />
+          <div className={styles.build}>
+            <RadioSection />
+            <hr className={styles.line} />
+            <section className={styles.row}>
+              <Metadata />
+              <Dependencies />
+            </section>
+          </div>
+          <Generate />
+        </div>
+      </Live>
+    </Room>
   )
 }
