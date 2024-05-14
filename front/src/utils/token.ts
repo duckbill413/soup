8a39401bc02909288f504dd25dfffcd54c0c79ff
최지우 <qlenfrl101@naver.com>
2024-05-14 13:44:32 +0900

    fix [FE] 토큰 에러 발생 시 로그아웃 로직 추가

--- front/src/utils/token.ts
+++ front/src/utils/token.ts
@@ -11,22 +11,30 @@ const setToken = (accessToken: any, refreshToken: any) => {
   setCookie(null, 'refreshToken', refreshToken)
 }
 
+const tokenClear = () => {
+  destroyCookie(null, ACCESS_TOKEN)
+  destroyCookie(null, REFRESH_TOKEN)
+}
+
 const tokenRefresh = async () => {
   const token = getRefreshToken()
+  console.log('내가 보낸 토큰========', token)
   const instance = axios.create({
     baseURL: process.env.NEXT_PUBLIC_BACKEND_BASE_URL,
   })
 
-  const res = await instance.post('/auth/token/refresh', {
-    refreshToken: token,
-  })
-  const data = res.data.result
-  setToken(data.accessToken, data.refreshToken)
-}
-
-const tokenClear = () => {
-  destroyCookie(null, ACCESS_TOKEN)
-  destroyCookie(null, REFRESH_TOKEN)
+  tokenClear()
+  try {
+    const res = await instance.post('/auth/token/refresh', {
+      refreshToken: token,
+    })
+    console.log('받은 res', res)
+    const data = res.data.result
+    setToken(data.accessToken, data.refreshToken)
+  } catch (e) {
+    console.log(e)
+    window.location.href = '/main'
+  }
 }
 
 export { getAccessToken, getRefreshToken, setToken, tokenClear, tokenRefresh }
