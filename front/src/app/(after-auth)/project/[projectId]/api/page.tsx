332118a5fbc7f4cb02170af36f34ba1b0785f1d1
최지우 <qlenfrl101@naver.com>
2024-05-16 15:45:16 +0900

    fix [FE] delete api sort menu

--- front/src/app/(after-auth)/project/[projectId]/api/page.tsx
+++ front/src/app/(after-auth)/project/[projectId]/api/page.tsx
@@ -2,7 +2,6 @@ import Room from '@/app/(after-auth)/project/[projectId]/api/Room'
 import { StepTitleWithGuide } from '@/components/StepTitle/StepTitle'
 import Live from '@/components/cursor/Live'
 import APITable from '@/containers/api/components/APITable'
-import SortMenu from '@/containers/api/components/SortMenu'
 
 export default function ApiSpecification() {
   return (
@@ -15,7 +14,6 @@ export default function ApiSpecification() {
             desc="ERD를 기반으로 API 명세서를 작성해보세요."
             guideTitle="API 명세서 작성 가이드"
           />
-          <SortMenu />
           <APITable />
         </div>
       </Live>
