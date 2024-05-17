227b211d0464cf29b43c790051d419987c840b33
정승원 <stevenjung1213@gmail.com>
2024-05-17 23:28:35 +0900

    style [FE] guide modal style
    - 모달 영역 스타일

--- front/src/components/GuideModal/GuideModal.tsx
+++ front/src/components/GuideModal/GuideModal.tsx
@@ -10,9 +10,8 @@ function GuideModal ({ children, onClose }:GuideProps) {
 
   return (
     <Draggable>
-      <div style={{ position: 'fixed', top: 0, left: 0, width: '100%', height: '100%', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-        <div style={{ position: 'absolute', padding: 20, background: 'white', boxShadow: '0 4px 8px rgba(0,0,0,0.1)' }}>
-
+      <div style={{ position: 'fixed', zIndex:1000, top: 0, left: 0, width: '40%', height: '40%', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+        <div style={{ width:'100%', height:'100%', position: 'absolute', padding: 20, background: 'white', boxShadow: '0 4px 8px rgba(0,0,0,0.1)' }}>
           <button type="button" onClick={onClose}>X</button>
           {children}
         </div>
